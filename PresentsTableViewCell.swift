ec265cd931ffab3a31dea86d0be2104ee8ef75e9
ElijahButers <Elijahbuters@gmail.com>
2017-01-19 20:18:53 +0200

    Added a PresentsTableViewCell class, a UITableViewCell subclass to the project.

---
+++ PresentsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PresentsTableViewCell.swift
+//  PresentBaseSwift3.0
+//
+//  Created by User on 1/19/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import UIKit
+
+class PresentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
